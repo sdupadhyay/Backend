199cf4e957c74e0358db9b8bcd0a7afe8fdc7185
sdupadhyay <104014308+sdupadhyay@users.noreply.github.com>
2024-01-30 00:17:38 +0530

    Mongo DB Connected with express

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connectDB = async () => {
+	try {
+		await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+		console.log("Mongo db connected");
+	} catch (err) {
+		console.log("Mongo DB ERROR:", err);
+	}
+};
+export default connectDB;
